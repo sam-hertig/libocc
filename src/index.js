8c240d59dcbd56fa68f6fa3d0d003721e8eb042e
Sam Hertig <sam@samhertig.com>
2018-06-03 19:03:07 +0200

    add dynamic group generation using d3s enter and exit statements, add d3.intervall, add CSS transitions

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const personHtml = `<path d="M49.693 89.133c-2.475 0-4.5-2.551-4.5-5.669V68.938c
                     3.119-2.025 5.67-4.5 5.67zM45.193 50.443V39.669c0-3.118 2.551-5.669 5.669-5.669h11.661c3.118 
                     0 5.669 2.551 5.669 5.669v10.774H45.193z"/>
                     <circle cx="56.693" cy="25.247" r="7"/>`
-const occGradient = `<radialGradient id="a" cx="56.693" cy="56.693" r="51.216" gradientUnits="userSpaceOnUse">
+const occupiedGradient = `<radialGradient id="a" cx="56.693" cy="56.693" r="51.216" gradientUnits="userSpaceOnUse">
                       <stop offset="0" stop-color="#cd5849"/>
                       <stop offset=".231" stop-color="#dc8a7f"/>
                       <stop offset=".49" stop-color="#ebbcb6"/>
@@ -50,38 +50,40 @@ const freeGradient = `<radialGradient id="b" cx="56.693" cy="56.693" r="51.216" 
 const labelG = "";
 
 
-////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Create base SVG
 const viz = d3.select("#viz")
-   .append("div")
-   .classed("svg-container", true)
-   .append("svg")
-   .attr("preserveAspectRatio", "xMinYMin meet")
-   .attr("viewBox", "0 0 " + width + " " + height)
-   .classed("svg-content-responsive", true)
-   .append("g")
-   .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); 
+    .append("div")
+    .classed("svg-container", true)
+    .append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 " + width + " " + height)
+    .classed("svg-content-responsive", true)
+    .on("click", () => update())   
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Fetch data
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log("Interactive visualization by Sam Hertig ––– www.samhertig.com");
-        console.log(data);
-        // if (!data.library_is_closed) {
-        plot(data);
-        // } else {
-
-        // }
-
-    })
-    .catch(e => {
-        console.log("Couldn't fetch library data from server;", e);
-    });
+// Main function
+const update = () => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // if (!data.library_is_closed) {
+            plot(data);
 
 
+        }).catch(e => {
+            console.log("Couldn't fetch library data from server;", e);
+        });
+}
+
+// Plot data
 const plot = data => {
+
+    console.log(data);
 
     // Randomize occupancy for testing
     data.G = Math.round(Math.random()*data.G_max);
@@ -95,7 +97,7 @@ const plot = data => {
 
 }
 
-
+// Plot single floor data
 const plotFloor = (label, count, max, level) => {
 
     // Place label
@@ -117,38 +119,96 @@ const plotFloor = (label, count, max, level) => {
         });
     }
 
-    // Place gradients
-    viz
-        .selectAll(".gradient")
+    // Update already placed occupied gradients
+    const occupiedGradientsClassName = "occupiedGradientOnFloor" + level;
+    const occupiedGradients = viz
+        .selectAll("." + occupiedGradientsClassName)
         .data(data)
-        .enter()
-        .append("g")
         .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
-        .attr("opacity", gradientOpacity)
-        .html(d => d.occ ? occGradient : freeGradient);
+        .attr("opacity", d => d.occ ? gradientOpacity : 0)
+        .html(occupiedGradient);
 
-    // Place desks
-    viz
-        .selectAll(".desk")
-        .data(data)
+    // Add new occupied gradients
+    occupiedGradients
         .enter()
         .append("g")
+        .attr("class", occupiedGradientsClassName + " gradient")
+        .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
+        .attr("opacity", d => d.occ ? gradientOpacity : 0)
+        .html(occupiedGradient);
+
+    // Remove occupied gradients if necessary
+    occupiedGradients.exit().remove();          
+
+    // Update already placed free gradients
+    const freeGradientsClassName = "freeGradientOnFloor" + level;
+    const freeGradients = viz
+        .selectAll("." + freeGradientsClassName)
+        .data(data)
+        .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
+        .attr("opacity", d => d.occ ? 0 : gradientOpacity)
+        .html(freeGradient);
+
+    // Add new free gradients
+    freeGradients
+        .enter()
+        .append("g")
+        .attr("class", freeGradientsClassName + " gradient")
+        .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
+        .attr("opacity", d => d.occ ? 0 : gradientOpacity)
+        .html(freeGradient);
+
+    // Remove free gradients if necessary
+    freeGradients.exit().remove();        
+
+    // Update already placed desks
+    const deskClassName = "deskOnFloor" + level;
+    const desks = viz
+        .selectAll("." + deskClassName)
+        .data(data)
         .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
         .html(deskHtml);
 
-    // Place people
-    viz
-        .selectAll(".person")
-        .data(data)
+    // Add new desks if needed
+    desks
         .enter()
         .append("g")
+        .attr("class", deskClassName)
+        .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
+        .html(deskHtml);
+
+    // Remove desks if necessary
+    desks.exit().remove();
+
+    // Update already placed people
+    const personClassName = "personOnFloor" + level;
+    const people = viz
+        .selectAll("." + personClassName)
+        .data(data)
         .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
         .attr("opacity", d => d.occ ? 1 : 0)
         .html(personHtml);
+
+    // Add new people if needed
+    people
+        .enter()
+        .append("g")
+        .attr("class", personClassName + " person")
+        .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
+        .attr("opacity", d => d.occ ? 1 : 0)
+        .html(personHtml);
+
+    // Remove people if necessary
+    people.exit().remove();
+
 }
 
+// Initialize and then update every minute
+update();
+d3.interval(update, 60000);
 
-
+// Console branding
+console.log("Interactive visualization by Sam Hertig ––– www.samhertig.com");
 
 
 
