4ea5c84c985f15d642acc0996139e8f851090e01
Sam Hertig <sam@samhertig.com>
2018-06-03 16:36:17 +0200

    continue working on icon display for all floors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import * as d3 from "d3";
 
+// API
 const url = "http://iz-websrv01.ethz.ch:3000/api/visitors";
 
 // Define dimensions
-const maxWidth = 600;
-const maxHeight = 600;
+const maxWidth = 400;
+const maxHeight = 400;
 const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 const width = maxWidth - margin.left - margin.right;
 const height = maxHeight - margin.top - margin.bottom;
 
-// Define spacings of icons
-const horMaxNrofIcons = 7;
+// Define size and spacings of icons
 const nrOfDesksPerIcons = 5;
-const iconScale = 0.3;
+const iconScale = 0.4;
 const gradientOpacity = 0.5;
-const iconHorSpace = 30;
-const iconVerSpace = 30;
-const floorVerSpace = 40;
+const iconHorSpace = 40;
+const iconVerSpace = 40;
+const floorVerSpace = 120;
+const offsetBottom = 80;
 
 // Icons and paths
 const deskHtml = `<path fill="#B7B7B8" d="M95.693 52.409h-78v9h10v34.5h9v-34.5h40v34.5h9v-34.5h10z"/>`
-const personHtml = `<path d="M49.693 89.133c-2.475 0-4.5-2.551-4.5-5.669V68.938c0-3.118 2.025-5.669 4.5-5.669s4.5 2.551 4.5 5.669v14.525c0 3.119-2.025 5.67-4.5 5.67zM63.693 89.133c-2.475 0-4.5-2.551-4.5-5.669V68.938c0-3.118 2.025-5.669 4.5-5.669s4.5 2.551 4.5 5.669v14.525c0 3.119-2.025 5.67-4.5 5.67zM45.193 50.443V39.669c0-3.118 2.551-5.669 5.669-5.669h11.661c3.118 0 5.669 2.551 5.669 5.669v10.774H45.193z"/>
+const personHtml = `<path d="M49.693 89.133c-2.475 0-4.5-2.551-4.5-5.669V68.938c0-3.118 2.025-5.669 4.5-5.669s4.5 
+                    2.551 4.5 5.669v14.525c0 3.119-2.025 5.67-4.5 5.67zM63.693 89.133c-2.475 
+                    0-4.5-2.551-4.5-5.669V68.938c0-3.118 2.025-5.669 4.5-5.669s4.5 2.551 4.5 5.669v14.525c0 
+                    3.119-2.025 5.67-4.5 5.67zM45.193 50.443V39.669c0-3.118 2.551-5.669 5.669-5.669h11.661c3.118 
+                    0 5.669 2.551 5.669 5.669v10.774H45.193z"/>
                     <circle cx="56.693" cy="25.247" r="7"/>`
 const occGradient = `<radialGradient id="a" cx="56.693" cy="56.693" r="51.216" gradientUnits="userSpaceOnUse">
                       <stop offset="0" stop-color="#cd5849"/>
@@ -41,6 +46,9 @@ const freeGradient = `<radialGradient id="b" cx="56.693" cy="56.693" r="51.216" 
                       </radialGradient>
                       <circle fill="url(#b)" cx="56.693" cy="56.693" r="51.216"/>`
 
+// Floor labels
+const labelG = "";
+
 
 ////////////////////////////////////////////////////////////
 
@@ -60,7 +68,13 @@ fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log("Interactive visualization by Sam Hertig ––– www.samhertig.com");
+        console.log(data);
+        // if (!data.library_is_closed) {
         plot(data);
+        // } else {
+
+        // }
+
     })
     .catch(e => {
         console.log("Couldn't fetch library data from server;", e);
@@ -69,58 +83,75 @@ fetch(url)
 
 const plot = data => {
 
-    // Randomize G-floor occupancy for testing:
+    // Randomize occupancy for testing
     data.G = Math.round(Math.random()*data.G_max);
+    data.H = Math.round(Math.random()*data.H_max);
+    data.J = Math.round(Math.random()*data.J_max);
 
-    // Generate data for G-floor
-    const nrOfdesksG = Math.round(data.G_max/10)*10;
-    const nrOfIconsG = nrOfdesksG/nrOfDesksPerIcons;
-    const dataFloorG = [];
-    for (let i = 0; i<nrOfIconsG; i++) {
+    // Plot floors individually
+    plotFloor(labelG, data.G, data.G_max, 0);
+    plotFloor(labelG, data.H, data.H_max, 1);
+    plotFloor(labelG, data.J, data.J_max, 2);
+
+}
+
+
+const plotFloor = (label, count, max, level) => {
+
+    // Place label
+
+    //TO DO
+
+
+    // Generate data with coordinates
+    const horMaxNrofIcons = Math.round(max/10);
+    const nrOfdesks = horMaxNrofIcons*10;
+    const nrOfIcons = nrOfdesks/nrOfDesksPerIcons;
+    const data = [];
+    for (let i = 0; i<nrOfIcons; i++) {
         const rowNr = Math.floor(i/horMaxNrofIcons);
-        dataFloorG.push({
+        data.push({
             x: i*iconHorSpace-(rowNr*horMaxNrofIcons*iconHorSpace),
-            y: rowNr*iconVerSpace,
-            occ: (i*nrOfDesksPerIcons)<data.G 
+            y: height - offsetBottom - (rowNr*iconVerSpace) - (level*floorVerSpace),
+            occ: (i*nrOfDesksPerIcons)<count 
         });
     }
 
-    // Place gradients for G-floor
-    const gradientsG = viz
+    // Place gradients
+    viz
         .selectAll(".gradient")
-        .data(dataFloorG)
+        .data(data)
         .enter()
         .append("g")
         .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
         .attr("opacity", gradientOpacity)
         .html(d => d.occ ? occGradient : freeGradient);
 
-    // Place desks for G-floor
-    const desksG = viz
+    // Place desks
+    viz
         .selectAll(".desk")
-        .data(dataFloorG)
+        .data(data)
         .enter()
         .append("g")
         .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
         .html(deskHtml);
 
-    // Place people for G-floor
-    const peopleG = viz
+    // Place people
+    viz
         .selectAll(".person")
-        .data(dataFloorG)
+        .data(data)
         .enter()
         .append("g")
         .attr("transform", d => "translate(" + d.x + "," + d.y + ") scale(" + iconScale + ")")
         .attr("opacity", d => d.occ ? 1 : 0)
         .html(personHtml);
-
 }
 
 
 
 
 
-   
+
 
 
 
