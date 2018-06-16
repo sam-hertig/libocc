c5f187284d0e4352c49593d47f12c9774a457fd0
Sam Hertig <sam@samhertig.com>
2018-06-16 16:40:27 +0200

    change lib icon to flat roof, change closed icon to einbahnstrasse, simplify business logic, ignore no-data flag, clean-up code

--- src/index.js
+++ src/index.js
@@ -1,50 +1,61 @@
 import * as d3 from "d3";
 
-const url = "http://iz-websrv01.ethz.ch:3000/api/visitors"; // API
-const debugMode = true; // mock API and generate dummy data
-const nrOfPeoplePerDesk = 5;
+const url = "http://iz-websrv01.ethz.ch:3000/api/visitors";
+const debugMode = false;
+const nrOfIcons = {
+	sittingPeopleG: 14,
+	sittingPeopleH: 14,
+	sittingPeopleJ: 8
+};
 let timer;
 
 
-const showUpdating = () => {
+// <text transform="matrix(1 0 0 1 1141 145)" fill="#808080" font-family="'HelveticaNeue-Light'" font-size="36" dy="0.35em"></text>
+const putText = (text, color="#808080", size=36) => {
     d3
         .selectAll("#ts > *")
-        .text("updating...")
-        .attr("class", "flashy")
-        .attr("dx", -12)
+        .text(text)
+        .attr("font-size", size)
+        .attr("fill", color)
         .on("click", () => update());
 }
 
-const showError = () => {
+
+const annotate = (closed, ts) => {
     d3
-        .selectAll("#ts > *")
-        .text("no data")
-        .attr("class", "")
-        .attr("dx", -12)
-        .on("click", () => update());
-}
+        .selectAll("#closed > #icon")
+        .attr("opacity", closed ? 1 : 0);
+    let text, color;
+    if (closed) {
+    	color = "#EC1C24";
+		text = "closed";
+    } else {
+        const formatTime = d3.timeFormat("%H:%M");
+    	text = formatTime(new Date(ts));
+    }
+    putText(text, color);
+};
+
 
 const processData = data => {
     console.log(data);
-    if (data.no_data) {
-        showError();
-        return;
-    }
-    refreshTimestamp(data.ts);
+    if (data.library_is_closed) {
+    	data.G = 0;
+		data.H = 0;
+		data.J = 0;
+		data.trend = 0;
+    } 
     refreshFloorOccupancy("sittingPeopleG", data.G, data.G_max);
     refreshFloorOccupancy("sittingPeopleH", data.H, data.H_max);
     refreshFloorOccupancy("sittingPeopleJ", data.J, data.J_max);
     visualizeTrend(data.trend);
-    libIsClosed(data.library_is_closed);
-    // !data.no_data) 
+    annotate(data.library_is_closed, data.ts);
 }
 
-// Main function
+
 const update = () => {
 
-    showUpdating();
-
-
+    putText("updating...", "#EC1C24", 22);
 
     if (!debugMode) {
         fetch(url)
@@ -52,7 +63,7 @@ const update = () => {
             .then(data => {
                 processData(data);
             }).catch(e => {
-                showError();
+                putText("no data", "#EC1C24", 30);
                 console.log("Couldn't fetch library data from server |", e);
             });        
     } else {
@@ -70,45 +81,20 @@ const update = () => {
         data.no_data = Math.random() > 0.7 && !data.library_is_closed;
         processData(data);       
     }
-
 }
 
 
-const libIsClosed = closed => {
-    d3
-        .selectAll("#closed > #icon")
-        .attr("opacity", closed ? 1 : 0);
-    if (closed) {
-        visualizeTrend(0);
-        d3
-            .selectAll("#ts > *")
-            .text("closed")
-            .attr("class", "")
-            .attr("dx", -5);            
-    } else {
-        d3
-            .selectAll("#ts > *")
-            .attr("class", "")
-            .attr("dx", 0);         
-    }
-};
-
-
 const visualizeTrend = trend => {
-
     if (timer) {
         timer.stop();
     }
-
     d3
         .selectAll("#walkingPerson")
         .interrupt()  
         .attr("opacity", 0);     
-
     if (trend === 0) {
         return;
     }
-
     const target = {
         x: trend === 1 ? 150 : 24,
         y: 499,
@@ -120,7 +106,6 @@ const visualizeTrend = trend => {
         scale: trend === 1 ? 2 : 1
     };
     const transtitionTime = 4000;
-
     const move = () => {
         d3
             .selectAll("#walkingPerson")
@@ -136,27 +121,13 @@ const visualizeTrend = trend => {
             .duration(0.2*transtitionTime)            
             .attr("opacity", 0);             
     }   
-    
     move();
     timer = d3.interval(move, transtitionTime);
 }
 
 
-// Update timestamp and insert refresh link
-const refreshTimestamp = (ts) => {
-    const formatTime = d3.timeFormat("%H:%M");
-    const timeStamp = formatTime(new Date(ts));
-    d3
-        .selectAll("#ts > *")
-        .text(timeStamp)
-        .attr("dx", 0)
-        .attr("class", "")
-        .on("click", () => update());
-};
-
-
-// Refresh occupancy for a single floor
 const refreshFloorOccupancy = (id, count, count_max) => {
+	const nrOfPeoplePerDesk = Math.round(count_max / nrOfIcons[id]);
     const maxCount = Math.round(count_max/nrOfPeoplePerDesk);
     const curCount = Math.round(count/nrOfPeoplePerDesk);
     const data = [];
@@ -171,11 +142,11 @@ const refreshFloorOccupancy = (id, count, count_max) => {
         .attr("opacity", d => d.occ ? 1 : 0)
 }
 
-// Initialize and then update every minute
+
 update();
 d3.interval(update, 60000);
 
-// Console branding
+
 console.log("Interactive visualization by Sam Hertig ––– www.samhertig.com");
 
 
